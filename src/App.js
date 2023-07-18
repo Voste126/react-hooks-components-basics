cb75048c3b18c03f6e6b23f6b16fdf27c0311725
voste126 <steveaustine126@gmail.com>
2023-07-18 12:35:34 +0300

    my first react working with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Article from "./Article";
 import Comment from "./Comment";
+import Nav from "./Nav";
 
 function App() {
   return (
     <div>
       <Article />
       <Comment />
+      <Nav />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <h1>This is my nav bar</h1>
+    );
+}
+
+export default Nav;
